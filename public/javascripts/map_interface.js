a4bfdb44f9dfb251a6590b54b09cff2ee156c1e8
Jonas Goslow <jonas@lyradesigns.com>
2015-01-10 10:26:39 -0800

    initial commit

---
+++ public/javascripts/map_interface.js
@@ -0,0 +1,172 @@
+$(function(){
+	var location = localStorage.getItem('location'),
+			radius = localStorage.getItem('radius');
+	if (location) {
+		if (!radius) { radius = 5; }
+		location = JSON.parse(location);
+		console.log(location, radius);
+		var latLng = {}; // Save Location for next visit
+		latLng.lat = location.k;
+		latLng.lng = location.D;
+		map.setLocation(latLng, radius*1000);
+		listings.get(location, radius, '', listings.display);
+	} else {
+		locationToolOpen();
+	}
+
+	$('#logo').click(function(){
+		localStorage.clear();
+		return false;
+	});
+
+
+	// Tools
+
+	// Show Listings
+	$('#show_listings').click(function(){
+		showHideListings();
+	});
+
+	function showHideListings() {
+		var l = $('#listings_wrap');
+		if (l.height() == '0') {
+			$("#show_listings .fa").html('&#xf102;');
+			$('#show_listings .text').html('Hide Listings');
+			TweenLite.to($('#show_listings'), .5, {right:317});
+			if ($('#location_tool .tools').is(':visible')) {
+				TweenLite.to($('#location_tool'), .5, {right:'70%'});
+				setTimeout(function(){
+					var h = $('#map_wrap').height() - 20;
+					TweenLite.to(l, .5, {height:h});
+				}, 500);
+			} else {
+				TweenLite.to($('#location_tool'), .5, {right:431});
+				setTimeout(function(){
+					var h = $('#map_wrap').height() - 20;
+					TweenLite.to(l, .5, {height:h});
+				}, 500);
+			}
+		} else {
+			TweenLite.to(l, .5, {height:0});
+			setTimeout(function(){
+				$("#show_listings .fa").html('&#xf103;');
+				$('#show_listings .text').html('Show Listings');
+				TweenLite.to($('#show_listings'), .5, {right:10});
+				if ($('#location_tool .tools').is(':visible')) {
+					TweenLite.to($('#location_tool'), .5, {right:'50%'});
+				} else {
+					TweenLite.to($('#location_tool'), .5, {right:132});
+				}
+			}, 500);
+		}
+	}
+	// Expand Map
+	$('#expand_map').click(function(){
+		var mw = $('#map_wrap, #map_canvas');
+		var h = $(window).height();
+		var w = $(window).width();
+		var mh = h - 120;
+		var mtt = $('#map_tools_top');
+		if (h > 430) {
+			if (mw.height() == 350) {
+				var w = mtt.width();
+				var w2 = $(window).width();
+				TweenLite.to(mw, .5, {height: mh});
+				$(this).html('&#xf102;');
+				TweenLite.set(mtt, {width: w, maxWidth: '100%'})
+				TweenLite.to(mtt, .5, {width: w2});
+				mtt.data('max-width', w).data('map','expanded');
+			} else {
+				var w = mtt.data('max-width');
+				mtt.data('map','reduced');
+				TweenLite.to(mw, .5, {height: 350});
+				$(this).html('&#xf103;');
+				TweenLite.to(mtt, .5, {width: w});
+				setTimeout(function(){
+					mtt.css('max-width', w).css('width','100%');
+				}, 500);
+				if ($('.map_modal_bg').css('display') == 'block') {
+					$('.map_modal_bg').fadeOut();
+					setTimeout(function(){
+						$('.map_modal_bg').css('opacity','.2');
+					}, 500);
+				}
+			}
+		}
+		mapResize();
+		return false;
+	});
+
+	$('#location_tool .icon').click(function(){
+		var w = $('#location_tool').width();
+		if (w < 40) {
+			locationToolOpen();
+		} else {
+			locationToolClose();
+		}
+	});
+	$('#location_tool .tools .close').click(function(){
+		locationToolClose();
+	});
+
+
+	// Keyboard Add Shortcuts to interface
+	$(document).keydown(function(e){
+	    if (e.keyCode == 70) {
+	    	if (!($(document.activeElement).is('input') || $(document.activeElement).is('textarea'))) {
+					if ($('#location_tool').width() == '330') {
+						locationToolClose();
+					} else {
+						locationToolOpen();
+					}
+	    	}
+	    }
+	    if (e.keyCode == 76) {
+	    	if (!($(document.activeElement).is('input') || $(document.activeElement).is('textarea'))) {
+				showHideListings();
+	    	}
+	    }
+			if (e.keyCode == 67) {
+				if (!($(document.activeElement).is('input') || $(document.activeElement).is('textarea'))) {
+					$('#map_new_listing').click();
+				}
+			}
+	});
+	$('.address_field').focus(function(){
+		$(this).attr('placeholder', '');
+	});
+
+
+	var area = $('#listings_wrap');
+	if (!area.is(':visible')) {
+  		/*area.show('slide', {direction: 'right'}, 1000);
+  		$('#map_canvas, #map_canvas > div, #map_canvas > div > div:first-child').each(function(){
+  			var w = $(this).width();
+  			$(this). animate({
+  				width: w - 300
+  			}, 1000);
+  		});*/
+  	}
+
+
+	// Location Tool
+	$('#location_filter').on('submit', function(){
+		var radius = mToK($('#radius_input').val()) * 1000,
+			address = $('#address_input').val(),
+			latLng;
+
+
+		getLocation(address, radius, 'getListings').done(function(){ //
+			locationToolClose();
+		});
+
+		return false;
+	});
+
+
+});
+
+function updateRadius(radius) {
+	document.querySelector('#radius_input').value = radius;
+	localStorage.setItem('radius', radius);
+}
---
+++ routes/listings.js
@@ -0,0 +1,126 @@
+var config = require('../config'); // DB Config
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+var dbURL = config.db.user+':'+config.db.pass+'@'+config.db.host+':'+config.db.port+'/'+config.db.db;
+var collections = ["listings", "users"];
+var db = require("mongojs").connect(dbURL, collections);
+var moment = require('moment');
+
+var mongoose = require('mongoose'),
+    passport = require('passport'),
+    Message = require('../lib/db/message-model'),
+    User = require('../lib/db/user-model'),
+    Listing = require('../lib/db/listing-model');
+
+moment().format();
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  var distance = parseInt(req.query.radius),
+      radius = 6371,
+      lat = parseFloat(req.query.lat),
+      lng = parseFloat(req.query.lng);
+
+  //console.log(distance, lat, lng)
+
+  // latitude boundaries
+  var maxlat = lat + rad2deg(distance / radius),
+      minlat = lat - rad2deg(distance / radius);
+  // longitude boundaries (longitude gets smaller when latitude increases)
+  var maxlng = lng + rad2deg(distance / radius / Math.cos(deg2rad(lat)));
+      minlng = lng - rad2deg(distance / radius / Math.cos(deg2rad(lat)));
+
+  //console.log('db vars: '+maxlat, minlat, maxlng, minlng);
+  db.listings.find({'latLng.lat': {$gt : minlat, $lt: maxlat}, 'latLng.lng': {$gt : minlng, $lt: maxlng}}).limit(5000, function(err, response) {
+    if (err) return console.log(err);
+    //console.log(response);
+    res.send(response);
+    //if (callback && typeof(callback) == "function") callback(response);
+  });
+});
+
+module.exports = router;
+
+
+/* GET New User page. */
+router.get('/new', function(req, res) {
+  var type = req.query.type;
+  res.render('listings/new', {
+    user: req.user,
+    title: 'Add Listing',
+    listingType: type
+  });
+});
+
+/* GET New User page. */
+router.get('/add', function(req, res) {
+  var listingInfo = req.query,
+      date = moment().format("YYYY-MM-DDTHH:mm:ssZ"),
+      newCreated = moment().format("YYYY-MM-DDTHH:mm:ssZ"),
+      newUpdated = moment().format("YYYY-MM-DDTHH:mm:ssZ"),
+      newExpires = moment(date, "YYYY-MM-DDTHH:mm:ssZ").add(1, 'years').format("YYYY-MM-DDTHH:mm:ssZ"); //'YYYY-MM-DD hh:mma'
+  var newLatLng = JSON.parse(decodeURI(listingInfo.latLng));
+  var newListing = new Listing({
+      owner: listingInfo.owner,
+      ownerName: listingInfo.ownerName,
+      type: listingInfo.type,
+      title: listingInfo.title,
+      description: listingInfo.description,
+      location: listingInfo.location,
+      city: listingInfo.city,
+      state: listingInfo.state,
+      zip: listingInfo.zip,
+      created: newCreated,
+      updated: newCreated,
+      expires: newExpires,
+      latLng: {
+        lat: newLatLng.lat,
+        lng: newLatLng.lng
+      },
+      active: true
+  });
+
+  newListing.save(function(err) {
+    if (err) {
+      console.log(err);
+      res.status(409).send(err);
+    } else {
+      console.log(newListing);
+      res.status(200).send('success', newListing);
+    }
+  });
+
+});
+
+
+
+
+
+function getListings(type, number, lat, lng, distance, res, callback) {
+  radius = 6371,
+  lat = lat,
+  lng = lng;
+
+  // latitude boundaries
+  var maxlat = lat + rad2deg(distance / radius),
+  minlat = lat - rad2deg(distance / radius);
+  // longitude boundaries (longitude gets smaller when latitude increases)
+  var maxlng = lng + rad2deg(distance / radius / Math.cos(deg2rad(lat)));
+  minlng = lng - rad2deg(distance / radius / Math.cos(deg2rad(lat)));
+
+  //console.log('db vars: '+maxlat, minlat, maxlng, minlng);
+  //'latLng.lat': {$gt : minlat, $lt: maxlat}, 'latLng.lng': {$gt : minlng, $lt: maxlng}
+  db.listings.find({}).limit(number, function(err, response) {
+    if (err) return console.log(err);
+    //console.log(response);
+    if (callback && typeof(callback) == "function") callback(response);
+  });
+}
+
+function rad2deg(angle) {
+  return angle * 57.29577951308232;
+}
+function deg2rad(angle) {
+  return angle * .017453292519943295;
+}
