8c1311ceb704c7f9bc8ea41c859b4face1f46371
Jonas Goslow <jonas@lyradesigns.com>
2015-01-22 17:27:42 -0800

    added sub pages and moved menu to separate file

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,32 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', {
-    title: 'Yards to Gardens',
+    title: 'Home',
+    bodyClasses: 'home',
+    user: req.user
+  });
+});
+
+/* GET About page. */
+router.get('/about', function(req, res) {
+  res.render('pages/about', {
+    title: 'About',
+    user: req.user
+  });
+});
+
+/* GET Resources page. */
+router.get('/resources', function(req, res) {
+  res.render('pages/resources', {
+    title: 'Resources',
+    user: req.user
+  });
+});
+
+/* GET Contact page. */
+router.get('/contact', function(req, res) {
+  res.render('pages/contact', {
+    title: 'Contact Us',
     user: req.user
   });
 });
